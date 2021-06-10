336c2c0f1a754d408f0a3bbb1fab61bd1d015f0b
Muhammad Usman <usman@getfastcode.com>
2021-06-10 14:53:49 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './ext-src/extention.ts',
+    mode: 'development',
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+    },
+    externals: {
+        'vscode': '@types/vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'extention.js',
+    },
+  };
